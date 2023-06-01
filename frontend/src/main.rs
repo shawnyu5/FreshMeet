4f31a5656947a6e11c42570f8ccd95f6b597e03b
shawn yu <shawn.yu5@hotmail.com>
2023-05-31 22:07:01 -0400

    work in progress

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,22 +1,78 @@
+use std::collections::HashMap;
+
+use dotenv::dotenv;
 use leptos::*;
+use leptos_router::*;
+use reqwest_wasm::Client;
 
 #[component]
 fn App(cx: Scope) -> impl IntoView {
-    view! { cx,
-        <p>"Hello, world!"</p>
+    log::info!("hello world");
+    view! {cx,
+      <Router>
+        <main>
+          <Routes>
+              <Route
+                  path="/"
+                  view=|cx| view! { cx, <Home/> }
+              />
+          </Routes>
+        </main>
+      </Router>
+    }
+}
+
+/// a generic button component
+#[component]
+fn Button(cx: Scope, label: String) -> impl IntoView {
+    view! {cx,
+        <div
+            style="class: button"
+            >
+            <button>{label}</button>
+        </div>
     }
 }
 
 #[component]
-fn Counter(cx: Scope) -> impl IntoView {
-    let (count, set_count) = create_signal(cx, 0);
-    view! {
+fn TechEvents(cx: Scope) -> impl IntoView {
+    let data = create_resource(
         cx,
-        <button
-            on:click=move |_| set_count(count.get() + 1)
-            >{ count }</button>
+        || (),
+        |_| async move {
+            log!("fetching data");
+            let mut map = HashMap::new();
+            // - `query`: the search query
+            // - `page`: page number to display
+            // - `per_page`: number of nodes to return
+            // - `start_date`: start date of events in ISO 8601 format
+            map.insert("query", "tech");
+            map.insert("page", "1");
+            map.insert("per_page", "10");
+            map.insert("start_date", "2023-05-31T00:00:00");
+
+            let res = Client::new()
+                .post("http://localhost:8000/meetup/search")
+                .json(&map)
+                .send()
+                .await
+                .unwrap();
+            return res.text().await.unwrap();
+        },
+    );
+}
+
+#[component]
+fn Home(cx: Scope) -> impl IntoView {
+    view! { cx,
+        <div>
+            <Button label="Hello".to_string() />
+            <TechEvents/>
+        </div>
     }
 }
+
 fn main() {
-    mount_to_body(|cx| view! { cx,  <Counter /> });
+    dotenv().ok();
+    mount_to_body(|cx| view! { cx,  <App/> });
 }
