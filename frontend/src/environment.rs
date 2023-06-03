3e69e540170d7206bfbc59b1d6c05e0355dace4e
shawn yu <shawn.yu5@hotmail.com>
2023-06-03 15:58:14 -0400

    feat: read api address from environment

--- frontend/src/environment.rs
+++ frontend/src/environment.rs
@@ -7,6 +7,5 @@ pub fn load() -> Environment {
     // dotenv::dotenv().ok();
     Environment {
         api_url: std::option_env!("API_URL").unwrap_or("http://localhost:8000"),
-        // api_url: std::env::var("API_URL").unwrap_or("http://localhost:8000".to_string()),
     }
 }
