53063f1f92ef89d11f4d9fd40f9e73f86f19eac7
shawn yu <shawn.yu5@hotmail.com>
2023-06-02 23:02:13 -0400

    feat: tech events page

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -35,35 +35,44 @@ fn Button(cx: Scope, label: String) -> impl IntoView {
     }
 }
 
-/// fetches events from the meetup api
+/// scrolls to the top of the page
+fn scroll_to_top() {
+    let window = web_sys::window().expect("Failed to access window object");
+    window.scroll_to_with_scroll_to_options(
+        web_sys::ScrollToOptions::new()
+            .top(0.0)
+            .behavior(web_sys::ScrollBehavior::Smooth),
+    )
+    // .expect("Failed to scroll to top");
+}
+/// fetches tech events from the meetup api
 #[component]
 fn TechEvents(cx: Scope) -> impl IntoView {
-    let page_number = use_context::<ReadSignal<u32>>(cx).expect("a read signal");
+    let after = use_context::<RwSignal<String>>(cx).expect("a String read write signal for after");
+    let page_number = use_context::<RwSignal<u32>>(cx).expect("a u32 read signal for page number");
 
-    let data: Resource<u32, Response> =
-        create_resource(cx, page_number.clone(), move |_| async move {
-            let page_number = page_number.get().to_string();
-            let page_number = page_number.as_str();
+    let data: Resource<u32, Response> = create_resource(cx, page_number, move |_| async move {
+        let mut map = HashMap::new();
+        let after_value = after.get();
+        let after_value = after_value.as_str();
 
-            log!("fetching data");
-            let mut map = HashMap::new();
-            map.insert("query", "tech");
-            map.insert("page", page_number);
-            map.insert("per_page", "10");
-            map.insert("start_date", "2023-05-31T00:00:00");
+        map.insert("query", "tech");
+        map.insert("after", after_value);
+        map.insert("per_page", "10");
 
-            let events = Client::new()
-                .post("http://localhost:8000/meetup/search")
-                .json(&map)
-                .send()
-                .await
-                .unwrap()
-                .json::<Response>()
-                .await
-                .unwrap();
+        let events = Client::new()
+            .post("http://localhost:8000/meetup/search")
+            .json(&map)
+            .send()
+            .await
+            .unwrap()
+            .json::<Response>()
+            .await
+            .unwrap();
 
-            return events;
-        });
+        after.set(events.page_info.endCursor.clone().unwrap_or_default());
+        return events;
+    });
 
     fn format_description(description: String) -> Vec<String> {
         return description.split("\n").map(|s| s.to_string()).collect();
@@ -96,7 +105,7 @@ fn TechEvents(cx: Scope) -> impl IntoView {
 fn NavBar(cx: Scope) -> impl IntoView {
     view! {cx,
         <div>
-            <button><A href="/">"Home"</A></button>
+            <button><A href="/">"Tech events"</A></button>
             <button><A href="/about">"About"</A></button>
         </div>
     }
@@ -105,12 +114,13 @@ fn NavBar(cx: Scope) -> impl IntoView {
 /// the home page
 #[component]
 fn Home(cx: Scope) -> impl IntoView {
-    let (page_number, set_page_number) = create_signal(cx, 2 as u32);
-    provide_context(cx, page_number);
-    provide_context(cx, set_page_number);
+    let page_signal = create_rw_signal(cx, 1 as u32);
+    let after_signal = create_rw_signal(cx, "".to_string());
+    provide_context(cx, page_signal);
+    provide_context(cx, after_signal);
 
     create_effect(cx, move |_| {
-        log!("page number changed to {}", page_number.get());
+        log!("page number changed to {}", page_signal.get());
     });
     // pass to components page number, and container of events.
     // every time page number changes, update events
@@ -128,24 +138,25 @@ fn Home(cx: Scope) -> impl IntoView {
 /// * `page_number`: the current page number
 #[component]
 fn Pagination(cx: Scope) -> impl IntoView {
-    let set_page_number = use_context::<WriteSignal<u32>>(cx).expect("a write signal");
+    let page_signal = use_context::<RwSignal<u32>>(cx).expect("a write signal");
 
     view! { cx,
     <div>
         <button
-            on:click = move |_| set_page_number.update(|value| {
+            on:click = move |_| page_signal.update(|value| {
                 if *value != 1 {
                     *value -= 1 as u32;
+                    scroll_to_top();
                 }
-
             })
             >
             "Previous"
         </button>
 
         <button
-            on:click = move |_| set_page_number.update(|value| {
+            on:click = move |_| page_signal.update(|value| {
                 *value += 1 as u32;
+                scroll_to_top();
             })
             >
             "Next"
