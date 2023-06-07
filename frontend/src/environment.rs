cd958f68cb09e92bc10fe44201a3189f26ec9d8c
shawn yu <shawn.yu5@hotmail.com>
2023-06-06 22:45:09 -0400

    fix(frontend): api url

--- frontend/src/environment.rs
+++ frontend/src/environment.rs
@@ -6,7 +6,7 @@ pub struct Environment {
 pub fn load() -> Environment {
     // dotenv::dotenv().ok();
     Environment {
-        api_url: "http://localhost:8000",
-        // api_url: std::option_env!("API_URL").unwrap_or("https://networking-accumulator.fly.dev/"),
+        // api_url: "http://localhost:8000",
+        api_url: std::option_env!("API_URL").unwrap_or("https://networking-accumulator.fly.dev/"),
     }
 }
