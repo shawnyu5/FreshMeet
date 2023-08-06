f9619b71523526c682c44335671713fad70b95a8
Shawn Yu <55592708+shawnyu5@users.noreply.github.com>
2023-08-06 17:01:27 -0400

    feat: use bulma css (#45)
    https://icon-icons.com/icon/meet-food-meat-pork-chops/55310

--- frontend/src/root.tsx
+++ frontend/src/root.tsx
@@ -15,6 +15,16 @@ import {
 import "./root.css";
 
 export default function Root() {
+  // <link
+  //   rel="stylesheet"
+  //   href="https://cdn.jsdelivr.net/npm/foundation-sites@6.7.5/dist/css/foundation.min.css"
+  //   crossorigin="anonymous"
+  // ></link>
+
+  // <script
+  //   src="https://cdn.jsdelivr.net/npm/foundation-sites@6.7.5/dist/js/foundation.min.js"
+  //   crossorigin="anonymous"
+  // ></script>
   return (
     <Html lang="en">
       <Head>
@@ -23,21 +33,15 @@ export default function Root() {
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
         <link
           rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/foundation-sites@6.7.5/dist/css/foundation.min.css"
-          crossorigin="anonymous"
-        ></link>
-
-        <script
-          src="https://cdn.jsdelivr.net/npm/foundation-sites@6.7.5/dist/js/foundation.min.js"
-          crossorigin="anonymous"
-        ></script>
+          href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"
+        />
       </Head>
       <Body>
         <Suspense>
           <ErrorBoundary>
             <A href="/meetup/tech">Tech</A>
             <A href="/meetup/bars">Bars</A>
-            <A href="/meetup/board-games">Board games / hangout</A>
+            <A href="/meetup/board-games">Board games + hangout</A>
             <Routes>
               <FileRoutes />
             </Routes>
