8922a14d9cf5ce30f1312962f2af745e04382847
shawn yu <shawn.yu5@hotmail.com>
2023-08-08 17:08:01 -0400

    fix: add graceful shutdown

--- axum-backend/src/main.rs
+++ axum-backend/src/main.rs
@@ -1,5 +1,7 @@
 mod meetup;
 mod routes;
+use tokio::signal;
+
 use crate::routes::app;
 use std::net::SocketAddr;
 
@@ -11,6 +13,32 @@ async fn main() {
 
     axum::Server::bind(&addr)
         .serve(app().into_make_service())
+        .with_graceful_shutdown(shutdown_signal())
         .await
         .unwrap();
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    println!("signal received, starting graceful shutdown");
 }
