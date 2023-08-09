8ab9284f8d3d77ffa3916d6f092c4a6c5699720f
shawn yu <shawn.yu5@hotmail.com>
2023-08-08 20:34:33 -0400

    fix(frontend): change page title

--- frontend/src/root.tsx
+++ frontend/src/root.tsx
@@ -28,7 +28,7 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>SolidStart - Bare</Title>
+        <Title>FreshMeet</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
         <link
