d4f1411c33c238dff4f3c8ab68d6623ab3ff13fb
shawn yu <shawn.yu5@hotmail.com>
2024-08-25 22:38:19 -0400

    fix(frontend): init date picker with correct date

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,87 +1,75 @@
 import "./NavBar.css";
 import "@rnwonder/solid-date-picker/dist/style.css";
 import DatePicker, { PickerValue } from "@rnwonder/solid-date-picker";
-import {
-   Accessor,
-   createSignal,
-   Setter,
-} from "solid-js";
+import { Accessor, createSignal, Setter } from "solid-js";
 
-export default function() {
-   let datetime = new Date();
-   // console.log(`Datetime day: ${datetime.getUTCDay()}`);
-   // // console.log(`Datetime utc: ${datetime.toUTCString()}`);
-   // console.log(`Datetime month: ${datetime.getUTCMonth()}`);
-   // console.log(`Datetime year: ${datetime.getUTCFullYear()}`);
-
-   const [dateRange, setDateRange] = createSignal<PickerValue>({
-      value: {
-         startDateObject: {
-            day: datetime.getDay(),
-            month: datetime.getMonth(),
-            year: datetime.getFullYear(),
-         },
-         endDateObject: {
-            day: datetime.getDay(),
-            month: datetime.getMonth(),
-            year: datetime.getFullYear(),
-         },
+export default function () {
+  let datetime = new Date();
+  const [dateRange, setDateRange] = createSignal<PickerValue>({
+    value: {
+      startDateObject: {
+        day: datetime.getDate(),
+        month: datetime.getMonth(),
+        year: datetime.getFullYear(),
       },
-      label: `${datetime.getFullYear()}-${datetime.getMonth()}-${datetime.getDay()}`,
-   });
+      endDateObject: {
+        day: datetime.getDate(),
+        month: datetime.getMonth(),
+        year: datetime.getFullYear(),
+      },
+    },
+    label: `${datetime.getFullYear()}-${datetime.getMonth()}-${datetime.getDate()}`,
+  });
 
-   return (
-      <div class="top-bar">
-         <style></style>
-         <div class="top-bar-left">
-            <ul class="dropdown menu" data-dropdown-menu>
-               <img src="../icon.png" width="50" />
-               <li class="menu-text">Fresh meat</li>
-               <li>
-                  <a href="/">Today</a>
-               </li>
-               <li>
-                  {
-                     // TODO: date picker goes here
-                  }
-                  <DatePickerComponent value={dateRange} setValue={setDateRange} />
-               </li>
-            </ul>
-         </div>
-
-         <div class="top-bar-right">
-            <ul class="menu">
-               <li>
-                  <input type="search" placeholder="Search"></input>
-               </li>
-               <li>
-                  <button type="button" class="button">
-                     Search
-                  </button>
-               </li>
-            </ul>
-         </div>
+  return (
+    <div class="top-bar">
+      <style></style>
+      <div class="top-bar-left">
+        <ul class="dropdown menu" data-dropdown-menu>
+          <img src="../icon.png" width="50" />
+          <li class="menu-text">Fresh meat</li>
+          <li>
+            <a href="/">Today</a>
+          </li>
+          <li>
+            <DatePickerComponent value={dateRange} setValue={setDateRange} />
+          </li>
+        </ul>
       </div>
-   );
+
+      <div class="top-bar-right">
+        <ul class="menu">
+          <li>
+            <input type="search" placeholder="Search"></input>
+          </li>
+          <li>
+            <button type="button" class="button">
+              Search
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 /**
  * A date picker component
  */
 function DatePickerComponent(props: {
-   value: Accessor<PickerValue>;
-   setValue: Setter<PickerValue>;
+  value: Accessor<PickerValue>;
+  setValue: Setter<PickerValue>;
 }) {
-   return (
-      <DatePicker
-         // onChange={(data) => {
-         //    if (data.type === "range") {
-         //       console.log(data.startDate, data.endDate);
-         //    }
-         // }}
-         type="range"
-         value={props.value}
-         setValue={props.setValue}
-      />
-   );
+  return (
+    <DatePicker
+      // onChange={(data) => {
+      //    if (data.type === "range") {
+      //       console.log(data.startDate, data.endDate);
+      //    }
+      // }}
+      type="range"
+      value={props.value}
+      setValue={props.setValue}
+    />
+  );
 }
