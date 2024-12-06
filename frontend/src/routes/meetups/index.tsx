fbd08699bfef801ddaeaf568a004adb408cd29fa
shawn yu <shawn.yu5@hotmail.com>
2024-12-05 22:18:44 -0500

    feat(frontend): align table row contents to the top of row instead of middle of row

--- frontend/src/routes/meetups/index.tsx
+++ frontend/src/routes/meetups/index.tsx
@@ -1,4 +1,5 @@
 import axios, { AxiosResponse } from "axios";
+import "./index.css"
 import {
   createEffect,
   createResource,
@@ -20,9 +21,6 @@ import { useSearchParams } from "@solidjs/router";
  */
 
 export default function () {
-  // const [pageNumber, setPageNumber] = createSignal(1);
-  // const [afterCursor, setAfterCursor] = createSignal("");
-  // const [getHasNextPage, setHasNextPage] = createSignal(true);
   const [searchParams, _setSearchParams] = useSearchParams();
   // Create a signal that is derived from searchParams
   const [paramsSignal, setParamsSignal] = createSignal(searchParams);
@@ -59,30 +57,6 @@ export default function () {
       return events;
     }
   });
-
-  // const scrollNext = async () => {
-  //   logger.info("Loading more events");
-  //   await refetchEvents();
-
-  // TODO: this does not work due to a framework issue
-  // https://github.com/solidjs/solid/issues/1864
-  // const new_events = await getMeetupsToday(afterCursor());
-  // mutateEvents((prev) => {
-  //   if (prev) {
-  //     logger.info("prev is not null");
-  //     console.log(prev.data.rankedEvents.edges);
-  //     const updated_edge = prev.data.rankedEvents.edges.concat(
-  //       new_events?.data.rankedEvents.edges as Array<Edge>
-  //     );
-
-  //     prev.data.rankedEvents.edges = updated_edge;
-  //     console.log(prev.data.rankedEvents.edges);
-  //     return prev;
-  //   } else {
-  //     return new_events as MeetupsToday;
-  //   }
-  // });
-  // };
 
   return (
     <Suspense fallback={<p>loading....</p>}>
