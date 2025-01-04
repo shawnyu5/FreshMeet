35d56ba1f6d10eefb7b024a903e56cd409b228d3
shawn yu <shawn.yu5@hotmail.com>
2025-01-03 22:26:34 -0500

    feat(frontend): make homepage not redirect for now

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { Navigate } from "@solidjs/router";
 
 export default function Home() {
-   return <Navigate href="/meetups"></Navigate>
+  return <p>hi</p>;
+  // return <Navigate href="/meetups"></Navigate>
 }
